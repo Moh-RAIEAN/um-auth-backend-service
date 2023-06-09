98794a50f63bed9044f53a8e74077670044db6fd
moh.raiean <hasibalraiean@gmail.com>
2023-06-09 09:33:16 +0600

    installed logger logger daily rotate file

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
 /* eslint-disable no-console */
-import colors from 'colors';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+import { errorLogger, infoLogger } from './shared/logger';
 
 async function main() {
   try {
     await mongoose.connect(config.databaseUrl as string);
-    console.log(colors.green.underline('database connected'));
+    infoLogger.info('database connected');
 
     app.listen(config.port, () => {
-      console.log(colors.green.underline(`server started on ${config.port}`));
+      infoLogger.info(`server started on ${config.port}`);
     });
   } catch (err) {
     if (err instanceof Error) {
-      console.log(colors.red(err.message));
+      errorLogger.error(err.message);
     }
   }
 }
---
+++ src/shared/logger.ts
@@ -0,0 +1,69 @@
+/* eslint-disable no-undef */
+import path from 'node:path';
+import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const { combine, timestamp, label, printf, prettyPrint } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  return `${date.toDateString()} ${hours}:${minutes}:${seconds}\n [${label}] ${level}: ${message}`;
+});
+
+const infoLogger = createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'hey dev' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'log',
+        'winston',
+        'successes',
+        '%phu-DATE%-success.log'
+      ),
+      datePattern: 'DD-MM-YYYY-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+});
+
+const errorLogger = createLogger({
+  level: 'error',
+  format: combine(
+    label({ label: 'hey dev' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'log',
+        'winston',
+        'errors',
+        'phu-%DATE%-errors.log'
+      ),
+      datePattern: 'DD-MM-YYYY-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+});
+
+export { infoLogger, errorLogger };
